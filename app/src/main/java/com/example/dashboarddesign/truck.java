0e0d0be09b9df7d6301d2c7768862b94e43fa965
xyz <123@gmail.com>
2020-09-05 19:30:36 -0700

    this is first commit for dashboard

---
+++ app/src/main/java/com/example/dashboarddesign/truck.java
@@ -0,0 +1,14 @@
+package com.example.dashboarddesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class truck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_truck);
+    }
+}
